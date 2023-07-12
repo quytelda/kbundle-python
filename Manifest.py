8a2b445783d22d61d0a5281125376a63d1382935
Quytelda Kahja <quytelda@tamalin.org>
2023-07-12 04:44:05 -0700

    Add constants for XML node names.

--- Manifest.py
+++ Manifest.py
@@ -1,6 +1,19 @@
 import pathlib
 import xml.dom.minidom as MD
 
+MANIFEST_PATH  = "META-INF/manifest.xml"
+MANIFEST_XMLNS = "urn:oasis:names:tc:opendocument:xmlns:manifest:1.0"
+
+# XML Node Names
+TAG_MANIFEST    = "manifest:manifest"
+TAG_FILE_ENTRY  = "manifest:file-entry"
+TAG_TAGS        = "manifest:tags"
+TAG_TAG         = "manifest:tag"
+ATTR_VERSION    = "manifest:version"
+ATTR_MEDIA_TYPE = "manifest:media-type"
+ATTR_FULL_PATH  = "manifest:full-path"
+ATTR_MD5SUM     = "manifest:md5sum"
+
 class Manifest:
 
     def __init__(self, manifestPath):
@@ -14,10 +27,10 @@ class Manifest:
         doc = MD.parse(self.path)
 
         root = doc.documentElement
-        if root.tagName != "manifest:manifest" or root.getAttribute("manifest:version") != "1.2":
+        if root.tagName != TAG_MANIFEST or root.getAttribute(ATTR_VERSION) != "1.2":
             return False;
 
-        for elem in doc.getElementsByTagName("manifest:file-entry"):
+        for elem in doc.getElementsByTagName(TAG_FILE_ENTRY):
             entry = self.__entry_from_xml(elem)
             if entry is None:
                 return False
@@ -59,21 +72,21 @@ class Manifest:
 
     def __tags_from_xml(self, e):
         tags = []
-        for tags_elem in e.getElementsByTagName("manifest:tags"):
-            for tag_elem in tags_elem.getElementsByTagName("manifest:tag"):
+        for tags_elem in e.getElementsByTagName(TAG_TAGS):
+            for tag_elem in tags_elem.getElementsByTagName(TAG_TAG):
                 tags.append(tag_elem.firstChild.data)
 
         return tags
 
     def __entry_from_xml(self, e):
-        if e.tagName != "manifest:file-entry":
+        if e.tagName != TAG_FILE_ENTRY:
             print("Invalid file entry.")
             return None
 
         entry = {
-            "media-type" : e.getAttribute("manifest:media-type"),
-            "full-path"  : e.getAttribute("manifest:full-path"),
-            "md5sum"     : e.getAttribute("manifest:md5sum"),
+            "media-type" : e.getAttribute(ATTR_MEDIA_TYPE),
+            "full-path"  : e.getAttribute(ATTR_FULL_PATH),
+            "md5sum"     : e.getAttribute(ATTR_MD5SUM),
             "tags"       : self.__tags_from_xml(e)
         }
 
