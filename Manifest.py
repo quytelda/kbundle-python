e67877c6f7a598255139bbb0e268f11ff87b3888
Quytelda Kahja <quytelda@tamalin.org>
2023-07-11 23:04:03 -0700

    Add skeleton Manifest class in Python.

---
+++ Manifest.py
@@ -0,0 +1,46 @@
+class Manifest:
+
+    def __init__(self):
+        pass
+
+    def exists(self):
+        pass
+
+    def load(self):
+        pass
+
+    def save(self):
+        pass
+
+    def insertEntry(self):
+        pass
+
+    def removeEntry(self):
+        pass
+
+    def hasEntry(self):
+        pass
+
+    def addTag(self):
+        pass
+
+    def removeTag(self):
+        pass
+
+    def toXML(self):
+        pass
+
+    def resourceList(self):
+        pass
+
+    def __tagListFromXML(self):
+        pass
+
+    def __fileEntryFromXML(self):
+        pass
+
+    def __tagListToXML(self):
+        pass
+
+    def __fileEntryToXML(self):
+        pass
