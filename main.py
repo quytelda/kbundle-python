a70b5e51611aecc84603a90630eba808dc53a5eb
Quytelda Kahja <quytelda@tamalin.org>
2023-07-13 11:09:19 -0700

    Add GPLv3 license and headers.

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 
+# Copyright 2023 Quytelda Kahja
+#
+# This file is part of kbundle.
+#
+# kbundle is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# kbundle is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with kbundle. If not, see <https://www.gnu.org/licenses/>.
+
 import bundle as B
 import sys
 import os
