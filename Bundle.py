514df689877d9c1d39b93ecb5ba8b409829249be
Quytelda Kahja <quytelda@tamalin.org>
2023-07-13 03:18:02 -0700

    Implement Bundle.scan_files().

--- Bundle.py
+++ Bundle.py
@@ -1,17 +1,43 @@
+import sys
 import os.path
 import Manifest
+
+RESOURCE_DIR_NAMES = ["brushes",
+                      "gamutmasks",
+                      "gradients",
+                      "paintoppresets",
+                      "palettes",
+                      "patterns",
+                      "seexpr_scripts",
+                      "workspaces"]
 
 class Bundle:
     def __init__(self, path):
         self.root = path
         manifest_path = self.__external_path(Manifest.MANIFEST_PATH)
         self.manifest = Manifest.Manifest(manifest_path)
-
-    def scan_files(self):
-        pass
+        self.resources = []
 
     def load(self):
         pass
+
+    def scan_files(self):
+        if not os.path.isdir(self.root):
+            print("Bundle directory does not exist: {}".format(self.root), file=sys.stderr)
+            return False
+
+        self.resources.clear()
+
+        for dirname in RESOURCE_DIR_NAMES:
+            dirpath = self.__external_path(dirname)
+            if not os.path.isdir(dirpath):
+                continue
+
+            for filename in os.listdir(dirpath):
+                filepath = os.path.join(dirpath, filename)
+                self.resources.append(filepath)
+
+        return True
 
     def prune_manifest(self):
         pass
