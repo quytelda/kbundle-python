c36427832fe17d53438eefaf57bdfe1528da8dc8
Quytelda Kahja <quytelda@tamalin.org>
2023-07-13 01:54:25 -0700

    Add skeleton Bundle class.

---
+++ Bundle.py
@@ -0,0 +1,35 @@
+import Manifest
+
+class Bundle:
+    def __init__(self, path):
+        pass
+
+    def scan_files(self):
+        pass
+
+    def load(self):
+        pass
+
+    def prune_manifest(self):
+        pass
+
+    def update_manifest(self):
+        pass
+
+    def add_tag(self):
+        pass
+
+    def remove_tag(self):
+        pass
+
+    def build(self):
+        pass
+
+    def __external_path(self):
+        pass
+
+    def __internal_path(self):
+        pass
+
+    def __zip_add_file(self):
+        pass
